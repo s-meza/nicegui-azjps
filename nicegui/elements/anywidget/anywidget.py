9f742603cd99a65b9500f1510ef388dac55ab230
Sofia Meza Salas <smeza@tower-research.com>
2025-12-01 04:33:15 +0800

    anywidget: Add TODOs

--- nicegui/elements/anywidget/anywidget.py
+++ nicegui/elements/anywidget/anywidget.py
@@ -52,18 +52,32 @@ class AnyWidget(ValueElement,
         super().__init__(value=traits, **kwargs)
 
         self._widget = widget
+        # TODO: Revisit comm implementation
+        #       - The comm should keep a GUID
+        #       - The widget should not render until it has received the comm open and created its own.
         self._widget.comm = aw_comm.create_comm(self)
 
         self._props['esm_content'], self._props['css_content'] = self.get_esm_css(widget)
+
         self._props['_debug'] = False  # set to True for console logging
 
         # Observe all widget traits and fire update_trait() JS method when changed in python
+        #----- TODO: The jupyter model is:
+        #              - There is no one true state.
+        #              - The state is duplicated across backend and frontend.
+        #              - When the state is changed, that end sends a comm to the other end
+        #            I think that this should be replaced by a send_state or similar.
+        #            References:
+        #              * https://github.com/jupyter-widgets/ipywidgets/blob/main/packages/base/src/widget.ts#L330
+        #              * https://ipywidgets.readthedocs.io/en/latest/examples/Widget%20Low%20Level.html#synchronized-state
         for trait in traits:
             def update_trait(change, trait=trait):
                 self.run_method('update_trait', {'trait': trait, 'new': change['new'], 'old': change['old']})
             self._widget.observe(update_trait, trait)
         self._update_method = 'update_traits'
+        #-----
 
+        # TODO: Different message for a state update?
         self.on('anywidget:msg', self._widget_send)
 
     def _widget_send(self, content: GenericEventArguments) -> None:
