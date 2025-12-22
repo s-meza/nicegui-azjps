1641f61739aa68680a2187a21ab4761a408b9827
Sofia Meza Salas <smeza@tower-research.com>
2025-12-22 09:14:08 +0800

    Add model.send tests

--- tests/test_anywidget.py
+++ tests/test_anywidget.py
@@ -5,7 +5,7 @@ from nicegui import ui
 from nicegui.testing import Screen
 
 
-def test_anywidget(screen: Screen):
+def test_anywidget_updates(screen: Screen):
     class CounterWidget(anywidget.AnyWidget):  # pylint: disable=abstract-method
         _esm = '''
             function render({ model, el }) {
@@ -35,3 +35,50 @@ def test_anywidget(screen: Screen):
     screen.click('anywidget: 42')
     screen.click('NiceGUI: 43')
     screen.should_contain('anywidget: 44')
+
+def test_anywidget_send(screen: Screen):
+    class MessageWidget(anywidget.AnyWidget):  # pylint: disable=abstract-method
+        _esm = '''
+            function render({ model, el }) {
+                const lambdas = [
+                  (e) => { e.innerText = "value:" + (model.get("value")) },
+                  (e) => { model.on("msg:custom", (msg) => { e.innerText = "custommsg:" + msg } ) },
+                  (e) => { model.on("change:value", (m, v) => { e.innerText = `m:${typeof m} newvalue:${v}` } ) },
+                  (e) => { model.send("message send") },
+                ]
+                for (const l of lambdas) {
+                  const p = document.createElement("p");
+                  p.innerText = "(nil)"
+                  l(p);
+                  el.appendChild(p)
+                }
+            }
+            export default { render };
+        '''
+        value = traitlets.Int(12).tag(sync=True)
+
+    msg = None
+    message_widget = MessageWidget()
+    def recv_message(_w, content, _buffers):
+        nonlocal msg
+        msg = content
+    message_widget.on_msg(recv_message)
+
+    @ui.page('/')
+    def page():
+        ui.anywidget(message_widget)
+
+    screen.open('/')
+
+    # Wait for widget to load
+    screen.wait(2)
+    message_widget.send('stuff')
+    message_widget.value = 50
+
+    # Wait for message to go through
+    screen.wait(1.5)
+
+    screen.should_contain('value:12')
+    screen.should_contain('custommsg:stuff')
+    screen.should_contain('m:object newvalue:50')
+    assert msg == 'message send'
